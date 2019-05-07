4f11947482edc5ba900fb3ba537eed20fcfad84b
Exaniaa <50368122+Exaniaa@users.noreply.github.com>
2019-05-07 15:08:51 +0200

    Add project files.

---
+++ Projet Final/Don.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+public class Objet
+{
+    string typeMateriel;
+    string refObj;
+    string descriComp;
+
+    public
+}
+
+public class Don : Objet
+{
+    DateTime dateRecep;
+    string nomDonnateur;
+    int numtel;
+    string adresse;
+    
+
+	public Don(DateTime dateRecep, string typeMateriel, string refObj, string nomDonnateur, int numtel, string adresse, string descriComp)
+	{
+        this.dateRecep = dateRecep;
+        this.nomDonnateur = nomDonnateur;
+        this.numtel = numtel;
+        this.adresse = adresse;
+	}
+
+    public override string ToString()
+    {
+        return "Date de reception : " + dateRecep.Day+"/"+dateRecep.Month+"/"+dateRecep.Year + "\nType de materiel : " + typeMateriel 
+            + "\nReference objet : " + refObj + "\nNom du donnateur : " + nomDonnateur + "\nNumero de telephone : " + numtel 
+            + "\nAdresse : " + adresse + "\nDescription complementaire : " + descriComp;
+    }
+
+    public static int Choix(Don don)
+    {
+        Console.WriteLine(don);
+        char readK;
+        Console.WriteLine();
+        Console.WriteLine("Accepter(A), Refuser(R) : ");
+        readK = Console.ReadKey().KeyChar;
+        if (readK == 'A')
+            return 1;
+        else
+            return 0;
+    } 
+}
+
+
---
+++ Projet Final/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projet_Final
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            int choix = 0;
+            DateTime dateRecep = new DateTime(2018, 10, 22);
+            Don don1 = new Don(dateRecep, "meuble", "C345", "Dumonnet", 0622334455, "Rue du Village", "");
+           
+            List<Don> Archives = new List<Don>();
+            List<Don> Accepte = new List<Don>();
+
+            choix = Don.Choix(don1);
+            if (choix == 1)
+                Accepte.Add(don1);
+            else
+                Archives.Add(don1);
+
+            Console.ReadKey();
+        }
+    }
+}
